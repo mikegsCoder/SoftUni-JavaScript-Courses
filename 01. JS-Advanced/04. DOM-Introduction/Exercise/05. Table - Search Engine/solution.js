1e3e8f78ae243124cc4e64668c7608a219046377
mikegsCoder <mikegsid@gmail.com>
2022-02-17 11:41:51 +0200

    Add new problem solutions in DOM Introduction - Exercise

---
+++ 01. JS-Advanced/04. DOM-Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -0,0 +1,17 @@
+function solve() {
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+    function onClick() {
+        let tableRowsElemets = Array.from(document.querySelectorAll('tbody tr'));
+        let searchedText = document.querySelector('#searchField').value;
+        console.log(searchedText);
+
+        for (const rowElement of tableRowsElemets) {
+            rowElement.classList.remove('select');
+
+            if (rowElement.textContent.includes(searchedText)) {
+                rowElement.classList.add('select')
+            }
+        }
+    }
+}
---
+++ 01. JS-Advanced/04. DOM-Introduction/Exercise/09. Number Convertor/solution.js
@@ -0,0 +1,33 @@
+function solve() {
+    let selectElement = document.querySelector('#selectMenuTo');
+    selectElement.selected = true;
+    selectElement.innerHTML = null;
+
+    let hexaElement = document.createElement('option');
+    let binaryElement = document.createElement('option');
+
+    hexaElement.value = 'hexadecimal';
+    hexaElement.textContent = 'Hexadecimal';
+
+    binaryElement.value = 'binary';
+    binaryElement.textContent = 'Binary';
+
+    selectElement.appendChild(hexaElement);
+    selectElement.appendChild(binaryElement);
+
+    document.querySelector('#container button').addEventListener('click', convert);
+
+    function convert() {
+        let number = Number(document.querySelector('#input').value);
+        let convertTo = {
+            'hexadecimal': function (number) {
+                return number.toString(16).toUpperCase();;
+            },
+            'binary': function (number) {
+                return (number >>> 0).toString(2);
+            }
+        }
+
+        document.querySelector('#result').value = convertTo[selectElement.value](number);
+    }
+}
