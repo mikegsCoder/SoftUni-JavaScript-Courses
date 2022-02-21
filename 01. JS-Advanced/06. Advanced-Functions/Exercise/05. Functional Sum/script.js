e49a6236e3bbd43d67e5cece95bb514eb91565dc
mikegsCoder <mikegsid@gmail.com>
2022-02-21 16:45:57 +0200

    Add new problem solutions in Advanced Functions - Exercise

---
+++ 01. JS-Advanced/06. Advanced-Functions/Exercise/05. Functional Sum/script.js
@@ -0,0 +1,15 @@
+function add(number) {
+    let sum = 0;
+
+    function inner(x) {
+        sum += x;
+
+        return inner;
+    }
+
+    inner.toString = () => sum;
+
+    return inner(number);
+}
+
+console.log(add(1)(5)(10).toString());
