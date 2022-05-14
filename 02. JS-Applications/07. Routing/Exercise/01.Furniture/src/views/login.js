033f950f4710761e769f17e0f3c2ff64597ff76c
mikegsCoder <mikegsid@gmail.com>
2022-05-14 12:54:20 +0300

    Minor changes in code formatting

--- 02. JS-Applications/07. Routing/Exercise/01.Furniture/src/views/login.js
+++ 02. JS-Applications/07. Routing/Exercise/01.Furniture/src/views/login.js
@@ -2,30 +2,29 @@ import { html } from "../dom.js";
 import api from "../api/api.js";
 
 const loginTemplate = () => html`
-    <div class="container">
+<div class="container">
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>Login User</h1>
+            <p>Please fill all fields.</p>
+        </div>
+    </div>
+    <form id="loginForm">
         <div class="row space-top">
-            <div class="col-md-12">
-                <h1>Login User</h1>
-                <p>Please fill all fields.</p>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label class="form-control-label" for="email">Email</label>
+                    <input class="form-control" id="email" type="text" name="email">
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="password">Password</label>
+                    <input class="form-control" id="password" type="password" name="password">
+                </div>
+                <input type="submit" class="btn btn-primary" value="Login" />
             </div>
         </div>
-        <form id="loginForm">
-            <div class="row space-top">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label class="form-control-label" for="email">Email</label>
-                        <input class="form-control" id="email" type="text" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label class="form-control-label" for="password">Password</label>
-                        <input class="form-control" id="password" type="password" name="password">
-                    </div>
-                    <input type="submit" class="btn btn-primary" value="Login" />
-                </div>
-            </div>
-        </form>
-    </div>
-`
+    </form>
+</div>`
 
 export function setupLogin() {
     return loginView;
