8756758c226a91ddaaed83b6e39ffa1c76208f56
mikegsCoder <mikegsid@gmail.com>
2022-05-20 17:30:14 +0300

    Add app.js, lib.js and util.js

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam Preparation 2 Cars Tube/Solution/src/app.js
@@ -0,0 +1,62 @@
+import { page, render } from './lib.js'
+import { getUserData } from './util.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/data.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { myCarsPage } from './views/my-cars.js';
+import { searchPage } from './views/search.js';
+
+// console.log('it works');
+// import * as api from './api/data.js';
+// window.api = api;
+
+const root = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+
+// page('/', () => console.log('home page'));
+
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/catalog', catalogPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/my-cars', myCarsPage);
+page('/search', searchPage);
+
+updateUserNav();
+page.start();
+page.redirect('/');
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('profile').style.display = 'inline-block';
+        document.getElementById('welcomeMsg').textContent = `Welcome ${userData.username}`
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('profile').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
