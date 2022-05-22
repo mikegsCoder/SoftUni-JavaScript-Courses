8bd5138b843f76ec6728e8aaeda03a9ad653cd70
mikegsCoder <mikegsid@gmail.com>
2022-05-22 13:23:32 +0300

    Add create, details and search views

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam 4 December 2021/Solution/src/views/details.js
@@ -0,0 +1,69 @@
+import { html, nothing } from '../lib.js';
+import { deleteAlbum, getAlbumById } from '../api/data.js';
+import { getUserData } from '../util.js';
+
+
+
+const detailsTemplate = (album, isOwner, onDelete) => html`
+<section id="detailsPage">
+    <div class="wrapper">
+        <div class="albumCover">
+            <img src=${album.imgUrl}>
+        </div>
+        <div class="albumInfo">
+            <div class="albumText">
+
+                <h1>Name: ${album.name}</h1>
+                <h3>Artist: ${album.artist}</h3>
+                <h4>Genre: ${album.genre}</h4>
+                <h4>Price: $${album.price}</h4>
+                <h4>Date: ${album.releaseDate}</h4>
+                <p>${album.description}</p>
+            </div>
+
+            ${albumControlsTemplate(album, isOwner, onDelete)}
+            <!-- Only for registered user and creator of the album-->
+            <!-- <div class="actionBtn">
+                <a href="#" class="edit">Edit</a>
+                <a href="#" class="remove">Delete</a>
+            </div> -->
+        </div>
+    </div>
+</section>`;
+
+const albumControlsTemplate = (album, isOwner, onDelete) => {
+    if (isOwner) {
+        return html`
+            <div class="actionBtn">
+                <a href="/edit/${album._id}" class="edit">Edit</a>
+                <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>
+            </div>`
+
+        // <a @click=${onDelete} href="javascript:void(0)" class="btn delete">Delete</a>
+        // <a href="/edit/${article._id}" class="btn edit">Edit</a>`
+
+    } else {
+        return nothing;
+    }
+};
+
+export async function detailsPage(ctx) {
+    //console.log('In Details')
+
+    const userData = getUserData();
+
+    const album = await getAlbumById(ctx.params.id);
+
+    const isOwner = userData && userData.id == album._ownerId;
+
+    ctx.render(detailsTemplate(album, isOwner, onDelete));
+
+    async function onDelete() {
+        const choice = confirm(`Are you sure you want to delete this album?`);
+
+        if (choice) {
+            await deleteAlbum(ctx.params.id);
+            ctx.page.redirect('/catalog');
+        }
+    }
+}
