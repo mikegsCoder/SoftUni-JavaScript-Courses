5e0223758d2ec6a5d0835a1a538d44a6c9b95b32
mikegsCoder <mikegsid@gmail.com>
2022-05-22 13:03:41 +0300

    Add home, login and register views

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam 4 December 2021/Solution/src/views/home.js
@@ -0,0 +1,23 @@
+import { html, nothing } from '../lib.js';
+// import { getMostRecentArticles } from '../api/data.js';
+
+
+
+const homeTemplate = () => html`
+<section id="welcomePage">
+    <div id="welcome-message">
+        <h1>Welcome to</h1>
+        <h1>My Music Application!</h1>
+    </div>
+
+    <div class="music-img">
+        <img src="./images/musicIcons.webp">
+    </div>
+</section>`;
+
+
+
+export async function homePage(ctx) {
+
+    ctx.render(homeTemplate());
+}
