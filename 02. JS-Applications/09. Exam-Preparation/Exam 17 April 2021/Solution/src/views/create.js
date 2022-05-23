e1ed0feab693367cdce898371685a0b32363dc8c
mikegsCoder <mikegsid@gmail.com>
2022-05-23 13:37:08 +0300

    Add catalog and create views

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam 17 April 2021/Solution/src/views/create.js
@@ -0,0 +1,71 @@
+import { html } from '../lib.js';
+import { createItem } from '../api/data.js';
+
+
+const createTemplate = (onSubmit) => html`
+<section id="create-page" class="create">
+    <form @submit=${onSubmit} id="create-form" action="" method="">
+        <fieldset>
+            <legend>Add new Pet</legend>
+            <p class="field">
+                <label for="name">Name</label>
+                <span class="input">
+                    <input type="text" name="name" id="name" placeholder="Name">
+                </span>
+            </p>
+            <p class="field">
+                <label for="description">Description</label>
+                <span class="input">
+                    <textarea name="description" id="description" placeholder="Description"></textarea>
+                </span>
+            </p>
+            <p class="field">
+                <label for="image">Image</label>
+                <span class="input">
+                    <input type="text" name="imageUrl" id="image" placeholder="Image">
+                </span>
+            </p>
+            <p class="field">
+                <label for="type">Type</label>
+                <span class="input">
+                    <select id="type" name="type">
+                        <option value="cat">Cat</option>
+                        <option value="dog">Dog</option>
+                        <option value="parrot">Parrot</option>
+                        <option value="reptile">Reptile</option>
+                        <option value="other">Other</option>
+                    </select>
+                </span>
+            </p>
+            <input class="button submit" type="submit" value="Add Pet">
+        </fieldset>
+    </form>
+</section>`;
+
+export function createPage(ctx) {
+    ctx.render(createTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const name = formData.get('name').trim();
+        const description = formData.get('description').trim();
+        const imageUrl = formData.get('imageUrl').trim();
+        const type = formData.get('type').trim();
+
+        //if( formData.values().some(x => x == ''))
+        if (name == '' || type == '' || description == '' || imageUrl == '') {
+            return alert('All fields are required!');
+        }
+
+        await createItem({
+            name,
+            description,
+            imageUrl,
+            type
+        });
+
+        ctx.page.redirect('/');
+    }
+}
