c6040f40df4fca2453df075a30174e70ae199b83
mikegsCoder <mikegsid@gmail.com>
2022-05-23 13:19:19 +0300

    Add app, lib and util.js

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam 17 April 2021/Solution/src/app.js
@@ -0,0 +1,56 @@
+import { page, render } from './lib.js'
+import { getUserData } from './util.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/data.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { profilePage } from './views/profile.js';
+
+// console.log('it works');
+// import * as api from './api/data.js';
+// window.api = api;
+
+const root = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+
+// page('/', () => console.log('home page'));
+
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/profile', profilePage);
+
+updateUserNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.getElementById('welcomeMsg').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
