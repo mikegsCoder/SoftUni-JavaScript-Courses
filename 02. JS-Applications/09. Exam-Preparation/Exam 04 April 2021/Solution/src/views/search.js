36da608bb6bba8f779f98763bc3bce556062d134
mikegsCoder <mikegsid@gmail.com>
2022-05-25 17:57:04 +0300

    Add search view

---
+++ 02. JS-Applications/09. Exam-Preparation/Exam 04 April 2021/Solution/src/views/search.js
@@ -0,0 +1,55 @@
+import { html, nothing } from '../lib.js';
+import { search } from '../api/data.js';
+
+
+const searchTemplate = (articles, onSearch) => html`
+<section id="search-page" class="content">
+    <h1>Search</h1>
+    <form @submit=${onSearch} id="search-form">
+        <p class="field search">
+            <input id="search-input" type="text" placeholder="Search by article title" name="search">
+        </p>
+        <p class="field submit">
+            <input class="btn submit" type="submit" value="Search">
+        </p>
+    </form>
+    <div class="search-container">
+
+        ${articles.length == 0
+        ? html`<h3 class="no-articles">No matching articles</h3>`
+        : articles.map(articlePreview)}
+
+    </div>
+</section>`;
+
+
+
+const articlePreview = (erticle) => html`
+<a class="article-preview" href="/details/${erticle._id}">
+    <article>
+        <h3>Topic: <span>${erticle.title}</span></h3>
+        <p>Category: <span>${erticle.category}</span></p>
+    </article>
+</a>`;
+
+export async function searchPage(ctx) {
+    //console.log('in search');
+
+    let articles = [];
+    ctx.render(searchTemplate(articles, onSearch));
+
+    async function onSearch(event) {
+        event.preventDefault();
+
+        const searchElement = document.getElementById('search-input');
+        const query = searchElement.value.trim();
+
+        if (query == '') {
+            return alert('Please write searcs string!')
+        }
+
+        articles = await search(query);
+        searchElement.value = '';
+        ctx.render(searchTemplate(articles, onSearch));
+    }
+}
