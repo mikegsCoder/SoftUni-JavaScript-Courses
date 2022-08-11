a776d7a0f827ce1284e4c953f908ba75b640ec01
mikegsCoder <mikegsid@gmail.com>
2022-08-11 12:32:37 +0300

    Included About component in App.js

--- 04. ReactJS/01. Intro-to-React-and-JSX/website-react-demo/src/App.js
+++ 04. ReactJS/01. Intro-to-React-and-JSX/website-react-demo/src/App.js
@@ -1,9 +1,11 @@
 import { HeroSection } from './components/HeroSection'
+import { About } from './components/About'
 
 function App() {
     return (
         <div>
             <HeroSection />
+            <About />
         </div>
     )
 }
