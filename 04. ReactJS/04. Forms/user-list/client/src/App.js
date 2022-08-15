12bde15f96759c3c37f3dc0f8cf421f215d7dbf7
mikegsCoder <mikegsid@gmail.com>
2022-08-15 16:46:54 +0300

    Add Footer component

--- 04. ReactJS/04. Forms/user-list/client/src/App.js
+++ 04. ReactJS/04. Forms/user-list/client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 </section>
             </main>
 
+            <Footer />
         </div>
     );
 }
