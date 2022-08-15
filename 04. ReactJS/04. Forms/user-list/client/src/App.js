43209a44705f51d4965685c490fc359069cfe90a
mikegsCoder <mikegsid@gmail.com>
2022-08-15 16:44:42 +0300

    Add Header component

--- 04. ReactJS/04. Forms/user-list/client/src/App.js
+++ 04. ReactJS/04. Forms/user-list/client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import { Header } from './components/common/Header';
+
 function App() {
     return (
         <div>
+            <Header />
 
             <main className="main">
                 <section className="card users-container">
