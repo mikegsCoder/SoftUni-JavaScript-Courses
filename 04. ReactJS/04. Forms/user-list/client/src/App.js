06809e9cf7d772c1f3c6c4effe7dcd44e05711d2
mikegsCoder <mikegsid@gmail.com>
2022-08-15 16:40:07 +0300

    Add user-list client

---
+++ 04. ReactJS/04. Forms/user-list/client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+    return (
+        <div>
+
+            <main className="main">
+                <section className="card users-container">
+
+                </section>
+            </main>
+
+        </div>
+    );
+}
+
+export default App;
