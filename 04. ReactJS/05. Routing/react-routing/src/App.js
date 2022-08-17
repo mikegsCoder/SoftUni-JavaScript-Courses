3dcc57941b730c59824b9a1dc1e43e3b17a90b9a
mikegsCoder <mikegsid@gmail.com>
2022-08-17 16:54:56 +0300

    Add Navigation in App.js

--- 04. ReactJS/05. Routing/react-routing/src/App.js
+++ 04. ReactJS/05. Routing/react-routing/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Navigation from './components/Navigation';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React Router</h1>
+      <Navigation />
 
     </div>
   );
