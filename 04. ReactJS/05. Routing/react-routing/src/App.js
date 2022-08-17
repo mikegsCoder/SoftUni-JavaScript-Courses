76bf07acf7e643d1c0d4b8ac35a4b5f7fa74a9a7
mikegsCoder <mikegsid@gmail.com>
2022-08-17 16:57:44 +0300

    Add Home component in App.js

--- 04. ReactJS/05. Routing/react-routing/src/App.js
+++ 04. ReactJS/05. Routing/react-routing/src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
+import './App.css'
 
-import Navigation from './components/Navigation';
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import Navigation from './components/Navigation'
+import Home from './components/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React Router</h1>
-      <Navigation />
+    return (
+        <div className="App">
+            <h1>Hello React Router</h1>
+            <Navigation />
 
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
