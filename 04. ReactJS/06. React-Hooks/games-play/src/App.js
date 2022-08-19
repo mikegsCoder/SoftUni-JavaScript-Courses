b80e2b541527b6cf5932e4a2ace68b48a24bb620
mikegsCoder <mikegsid@gmail.com>
2022-08-19 14:13:45 +0300

    Add games-play app

---
+++ 04. ReactJS/06. React-Hooks/games-play/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div id="box">
+            <main id="main-content">
+                
+            </main>
+        </div>
+    )
+}
+
+export default App
