81815a00232359376d186777a97323dc494e5375
mikegsCoder <mikegsid@gmail.com>
2022-08-19 14:24:05 +0300

    Add AuthContext and GameContext in App.js

--- 04. ReactJS/06. React-Hooks/games-play/src/App.js
+++ 04. ReactJS/06. React-Hooks/games-play/src/App.js
@@ -1,12 +1,23 @@
 import './App.css'
 
+import { useEffect, useState, lazy, Suspense } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
+import * as gameService from './services/gameService'
+import { AuthContext } from './contexts/AuthContext'
+import { GameContext } from './contexts/GameContext'
+
 function App() {
     return (
-        <div id="box">
-            <main id="main-content">
-                
-            </main>
-        </div>
+        <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
+            <div id="box">
+                <GameContext.Provider value={{ games, gameAdd, gameEdit }}>
+                    <main id="main-content">
+                        
+                    </main>
+                </GameContext.Provider>
+            </div>
+        </AuthContext.Provider>
     )
 }
 
