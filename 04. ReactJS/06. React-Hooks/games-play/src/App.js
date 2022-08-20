93003143fb1c088ff79d106e230cde8972bf528b
mikegsCoder <mikegsid@gmail.com>
2022-08-20 09:11:33 +0300

    Add functionality in App.js

--- 04. ReactJS/06. React-Hooks/games-play/src/App.js
+++ 04. ReactJS/06. React-Hooks/games-play/src/App.js
@@ -10,6 +10,52 @@ import { GameContext } from './contexts/GameContext'
 import Header from './components/Header/Header';
 
 function App() {
+    const [games, setGames] = useState([]);
+    const [auth, setAuth] = useLocalStorage('auth', {});
+    const navigate = useNavigate();
+
+    const userLogin = (authData) => {
+        setAuth(authData);
+    };
+
+    const userLogout = () => {
+        setAuth({});
+    };
+
+    const addComment = (gameId, comment) => {
+        setGames(state => {
+            const game = state.find(x => x._id == gameId);
+
+            const comments = game.comments || [];
+            comments.push(comment)
+
+            return [
+                ...state.filter(x => x._id !== gameId),
+                { ...game, comments },
+            ];
+        });
+    };
+
+    const gameAdd = (gameData) => {
+        setGames(state => [
+            ...state,
+            gameData,
+        ]);
+
+        navigate('/catalog');
+    };
+
+    const gameEdit = (gameId, gameData) => {
+        setGames(state => state.map(x => x._id === gameId ? gameData : x));
+    }
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result);
+            });
+    }, []);
+    
     return (
         <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
             <div id="box">
