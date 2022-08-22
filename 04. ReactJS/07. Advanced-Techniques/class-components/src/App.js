617d58c00f1a38a6f24e63e76a211ad49fa75167
mikegsCoder <mikegsid@gmail.com>
2022-08-22 17:02:16 +0300

    Add TaskList in App.js

--- 04. ReactJS/07. Advanced-Techniques/class-components/src/App.js
+++ 04. ReactJS/07. Advanced-Techniques/class-components/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import './App.css'
+
+import { Component } from 'react'
+import TaskList from './components/TaskList'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <main className="App-header">
-                    
+                    <h1>ToDo List</h1>
+                    <TaskList />
                 </main>
             </div>
-        );
+        )
     }
 }
 
-export default App;
+export default App
