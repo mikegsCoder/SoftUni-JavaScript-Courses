985172884f03278afe84b38ec8560b497056c3a4
mikegsCoder <mikegsid@gmail.com>
2022-08-22 16:53:50 +0300

    Add class-component app

---
+++ 04. ReactJS/07. Advanced-Techniques/class-components/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <main className="App-header">
+                    
+                </main>
+            </div>
+        );
+    }
+}
+
+export default App;
