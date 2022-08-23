56d4d6ea8242087d16bc53c2c941230aaac64ecf
mikegsCoder <mikegsid@gmail.com>
2022-08-23 15:57:45 +0300

    Add request to take characters from swapi.dev in App.js

--- 04. ReactJS/07. Advanced-Techniques/optimized-react/src/App.js
+++ 04. ReactJS/07. Advanced-Techniques/optimized-react/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
- 
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      fetch('https://swapi.dev/api/people')
+        .then(res => res.json())
+        .then(result => {
+          setCharacters(result.results);
+        })
+    }, []);
+    
   return (
     <div className="App">
       <header className="App-header">
-        
+
       </header>
     </div>
   );
